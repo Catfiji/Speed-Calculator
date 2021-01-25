7835287bcd4f111e1cb49c0cdb30c83b944ff4f2
Cathonia <68382607+Catfiji@users.noreply.github.com>
2021-01-25 18:32:13 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 // intit
-void speed(void);
+    void speed(void)
+;
 //main
-int main(void) {
-  speed();
-  return 0;
+int main(void) 
+{   speed()
+;   return 0
+;
 }
 //main formula
-void speed(void){
-    int D;
-    int T;
-  
-    printf("Distance:\n");
-    scanf ("%d",&D);
-    printf("Time: \n");
-    scanf("%d",&T);
-  
-    int answer = D / T;
-    printf("%d km/h\n",answer);
+void speed(void)
+{   int D
+;   int T
+;   printf("Distance:\n")
+;   scanf ("%d",&D)
+;   printf("Time: \n")
+;   scanf("%d",&T)
+;   int answer = D / T
+;   printf("%d km/h\n",answer)
+;
 }
